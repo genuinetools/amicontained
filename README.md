852ff984233389c5bf9ae357cbc06118f71a7cea
Jess Frazelle <acidburn@google.com>
2017-08-03 21:56:09 -0400

    initial commit
    Signed-off-by: Jess Frazelle <acidburn@google.com>

---
+++ README.md
@@ -0,0 +1,24 @@
+# amicontained
+
+[![Travis CI](https://travis-ci.org/jessfraz/amicontained.svg?branch=master)](https://travis-ci.org/jessfraz/amicontained)
+
+Container introspection tool. Find out what container runtime is being used as
+well as features available.
+
+## Installation
+
+#### Binaries
+
+- **linux** [386](https://github.com/jessfraz/amicontained/releases/download/v0.0.0/amicontained-linux-386) / [amd64](https://github.com/jessfraz/amicontained/releases/download/v0.0.0/amicontained-linux-amd64) / [arm](https://github.com/jessfraz/amicontained/releases/download/v0.0.0/amicontained-linux-arm) / [arm64](https://github.com/jessfraz/amicontained/releases/download/v0.0.0/amicontained-linux-arm64)
+
+#### Via Go
+
+```bash
+$ go get github.com/jessfraz/amicontained
+```
+
+## Usage
+
+```console
+$ amicontained -h
+```
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/jessfraz/amicontained/version"
+)
+
+const (
+	// BANNER is what is printed for help/info output
+	BANNER = `                 _                 _        _                _
+  __ _ _ __ ___ (_) ___ ___  _ __ | |_ __ _(_)_ __   ___  __| |
+ / _` + "`" + ` | '_ ` + "`" + ` _ \| |/ __/ _ \| '_ \| __/ _` + "`" + ` | | '_ \ / _ \/ _` + "`" + ` |
+| (_| | | | | | | | (_| (_) | | | | || (_| | | | | |  __/ (_| |
+ \__,_|_| |_| |_|_|\___\___/|_| |_|\__\__,_|_|_| |_|\___|\__,_|
+ Container introspection tool.
+ Version: %s
+
+`
+)
+
+var (
+	debug bool
+	vrsn  bool
+)
+
+func init() {
+	// parse flags
+	flag.BoolVar(&vrsn, "version", false, "print version and exit")
+	flag.BoolVar(&vrsn, "v", false, "print version and exit (shorthand)")
+	flag.BoolVar(&debug, "d", false, "run in debug mode")
+
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, fmt.Sprintf(BANNER, version.VERSION))
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	// set log level
+	if debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	if vrsn {
+		fmt.Printf("amicontained version %s, build %s", version.VERSION, version.GITCOMMIT)
+		os.Exit(0)
+	}
+
+	if flag.NArg() < 1 {
+		return
+	}
+
+	// parse the arg
+	arg := flag.Args()[0]
+
+	if arg == "help" {
+		usageAndExit("", 0)
+	}
+
+	if arg == "version" {
+		fmt.Printf("amicontained version %s, build %s", version.VERSION, version.GITCOMMIT)
+		os.Exit(0)
+	}
+}
+
+func main() {
+}
+
+func usageAndExit(message string, exitCode int) {
+	if message != "" {
+		fmt.Fprintf(os.Stderr, message)
+		fmt.Fprintf(os.Stderr, "\n\n")
+	}
+	flag.Usage()
+	fmt.Fprintf(os.Stderr, "\n")
+	os.Exit(exitCode)
+}
